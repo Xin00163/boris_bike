a3656a4c89740d48114967d6025f6cdf87e87483
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-22 22:09:27 +0000

    Change DockingStation to an array

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -2,16 +2,18 @@ class DockingStation
   attr_reader :bike
 
   def initialize
-    @bike = nil
+    @bike = []
   end
 
   def release_bike
-    raise 'There are no bikes' if @bike == nil
-    @bike
+    raise 'There are no bikes' if @bike.length == 0
+
+    @bike.pop()
   end
 
   def dock(bike)
-    @bike = bike
+    @bike.push(bike)
   end
+
 
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,21 +4,24 @@ require 'bike'
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike, :dock)}
 
-  it "realease a bike and it's working" do
+  it "release a bike and it's working" do
     newbike = Bike.new
     subject.dock(newbike)
-    expect(subject.release_bike).to eq(newbike)
-    expect(newbike.working?).to be(true)
+    released_bike = subject.release_bike
+    expect(released_bike).to eq(newbike)
+    expect(released_bike.working?).to be(true)
   end
 
-  it 'returns a docked bike' do
+  it 'docks a bike' do
     bike = Bike.new
-    expect(subject.dock(bike)).to be_a Bike
+    expect(subject.dock(bike)).to be_a Array
   end
 
-  it "raise an error" do
-    station = DockingStation.new
-    expect { station.release_bike}.to raise_error("There are no bikes")
+  it 'releases only as many bikes as there are docked bikes' do
+    newbike = Bike.new
+    expect(subject.dock(newbike)).to be_a Array
+    expect(subject.release_bike).to eq(newbike)
+    expect { subject.release_bike}.to raise_error("There are no bikes")
   end
 
 end
