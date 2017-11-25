a094305311e8fefde1b252a1b326c425a8277d76
Xin Wang <wangxin00163@yahoo.co.uk>
2017-11-25 00:13:07 +0000

    Step19 & 20

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -4,35 +4,37 @@ require 'docking_station'
 describe DockingStation do
   it {is_expected.to respond_to(:release_bike, :dock)}
 
-  it 'docks a bike' do
-    bike = Bike.new
-    expect(subject.dock(bike)).to be_a Array
-  end
 
   describe '#release_bike' do
     it 'releases only as many bikes as there are docked bikes' do
       expect {subject.release_bike}.to raise_error("There are no bikes")
     end
     it 'will not release broken bikes' do
-      bike = Bike.new
-      bike.report_broken
+      bike = double(:bike)
+      allow(bike).to receive(:broken?).and_return(true)
       subject.dock(bike)
       expect{subject.release_bike}.to raise_error "There are no bikes"
     end
   end
 
   describe '#dock' do
+
+    it 'docks a bike' do
+      bike = double(:bike)
+      allow(bike).to receive(:broken?).and_return(true)
+      expect(subject.dock(bike)).to be_a Array
+    end
+
     it "raise an error message when docking station is full" do
+      bike = double(:bike)
       subject.capacity.times do
-        subject.dock(Bike.new)
+        allow(bike).to receive(:broken?).and_return(true)
+        subject.dock(bike)
       end
-      expect {subject.dock(Bike.new)}.to raise_error("Docking station is full")
+      expect {subject.dock(bike)}.to raise_error("Docking station is full")
     end
   end
 
-  # it "should be able to change capacity when necessary" do
-  #   expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
-  # end
 
   it "should be able to set a different capacity" do
     expect(subject.capacity).to eq DockingStation::DEFAULT_CAPACITY
